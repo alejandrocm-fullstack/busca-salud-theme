bdb35c41f87ef40227bb1f523f691df58b330c3c
Alejandro CM <moral.cazorla@gmail.com>
2026-01-19 19:41:50 +0000

    Agrego carpeta ultimate-member

---
+++ ultimate-member/email/suspicious-activity.php
@@ -0,0 +1,12 @@
+<div style="max-width: 560px;padding: 20px;background: #ffffff;border-radius: 5px;margin: 40px auto;font-family: Open Sans,Helvetica,Arial;font-size: 15px;color: #666">
+<div style="color: #444444;font-weight: normal">
+<div style="text-align: center;font-weight: 600;font-size: 26px;padding: 10px 0;border-bottom: solid 3px #eeeeee">{site_name}</div>
+<div style="clear: both"> </div>
+</div>
+<div style="padding: 0 30px 30px 30px;border-bottom: 3px solid #eeeeee">
+<div style="padding: 30px 0;font-size: 14px">Le informamos que hay actividades sospechosas con la(s) siguiente(s) cuenta(s):</div>
+<div style="padding: 30px 0;font-size: 14px">{banned_profile_links}</div>
+<div style="padding: 30px 0;font-size: 14px">Debido a eso, hemos establecido el estado de cada cuenta(s) como rechazada o desactivada, hemos revocado roles y hemos destruido la sesión de inicio de sesión.</div>
+</div>
+<div style="color: #999;padding: 20px 30px">- Enviado a través del complemento Ultimate Member.</div>
+</div>
